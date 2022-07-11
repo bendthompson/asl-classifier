2e7c061070fa896d0faf0c2e14217d400b45fae2
ben thompson <ben97thompson@gmail.com>
2022-07-11 20:38:34 +0900

    structured app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
 from keras.models import load_model
-
-best_model = load_model('bestmodel.h5')
-
-st.title("American Sign Language Classifier👋")
-
+from tensorflow.keras.preprocessing.image import img_to_array
 
 #tagline
 #explain &links
@@ -15,3 +11,17 @@ st.title("American Sign Language Classifier👋")
 #model.predict
 #is it correct
 #dict image
+
+best_model = load_model('bestmodel.h5')
+
+st.title("ASL Classifier👋")
+st.subheader("This is a deep learning app that classifies which American Sign Language alphabet sign is in an image")
+st.write("yo yo im ben")
+
+
+upload = st.file_uploader(label='Upload an Image',type=['jpg','png'])
+
+if upload:
+    #send image to model
+    #output prediction
+    pass
