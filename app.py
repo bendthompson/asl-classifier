c4f595663bf4c5edb120b348fdca297e6240c572
ben thompson <ben97thompson@gmail.com>
2022-07-11 17:39:10 +0900

    started streamlit file

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from keras.models import load_model
+
+best_model = load_model('bestmodel.h5')
+
+st.title("American Sign Language Classifier👋")
+
+
+#tagline
+#explain &links
+#load example images
+#display example images
+#input image
+#model.predict
+#is it correct
+#dict image
